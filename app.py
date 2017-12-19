2ff9ea9fcb3f590c0fbd58c84d07e91a9155761c
Daniel <daniel.verdugo521@gmail.com>
2017-12-19 00:07:38 -0800

    Just slapping on a few things

--- app.py
+++ app.py
@@ -10,9 +10,19 @@ def index():
 def cakes():
     return render_template('cakes.html')
 
-@app.route('/hello/<name>')
-def hello(name):
-    return render_template('page.html', name=name)
+@app.route('/sarah')
+def sarah():
+    return render_template('sarah.html')
+
+@app.route('/daniel')
+def daniel():
+    return render_template('daniel.html')
+
+@app.route('/juliette')
+def juliette():
+    return render_template('juli.html')
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
